1c25beb8b46042e9fc25775ca1ea2e0751e21df0
marmooo <marmooo@users.noreply.github.com>
2023-03-24 10:10:25 +0900

    fix unlockAudio()

--- src/index.js
+++ src/index.js
@@ -857,7 +857,10 @@ function enableController() {
 }
 
 function unlockAudio() {
+  if (!player) return;
+  if (!player.synth) return;
   player.resumeContext();
+  document.removeEventListener("click", unlockAudio);
 }
 
 function play() {
@@ -1434,7 +1437,4 @@ document.getElementById("inputSoundFontUrl").onchange = loadSoundFontUrlEvent;
 document.getElementById("soundfonts").onchange = changeConfig;
 document.getElementById("courseOption").onchange = changeButtons;
 window.addEventListener("resize", resize);
-document.addEventListener("click", unlockAudio, {
-  once: true,
-  useCapture: true,
-});
+document.addEventListener("click", unlockAudio);
